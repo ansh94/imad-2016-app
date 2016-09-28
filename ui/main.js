0c9c9dbea5dea00c475e1c8d4d10226f6136438f
Anshdeep Singh <anshdeep.1494@gmail.com>
2016-09-28 13:15:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,14 @@ element.innerHTML = 'New Value';
 
 //move the image
 var image = document.getElementById("madi");
+var marginLeft = 0;
+
+
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    image.style.marginLeft = marginLeft + 'px';
+}
+
 image.onclick = function(){
-    image.style.marginLeft = "100px";
+    var interval = setInterval(moveRight,100);
 }
